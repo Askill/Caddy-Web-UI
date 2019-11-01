eb712716a6c1d4ada95dbdd11261f530ce788b27
Askill <ofjokg@gmail.com>
2019-11-01 16:08:37 +0100

    todo: fom data to json

--- static/js/main.js
+++ static/js/main.js
@@ -12,24 +12,61 @@ var getJSON = function(url, callback) {
     };
     xhr.send();
 };
+var postJSON = function(url, data, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', url, true);
+    xhr.responseType = 'json';
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.onload = function() {
+      var status = xhr.status;
+      if (status === 200) {
+        callback(null, xhr.response);
+      } else {
+        callback(status, xhr.response);
+      }
+    };
+    xhr.send(data);
+};
 
 function loadSites(){
     getJSON('http://localhost:8000/api/Sites',
         function(err, data) {
+            data = Object.values(data);
             if (err !== null) {
                 alert('Something went wrong: ' + err);
             } else {
-                for (index = 0; index < Object.keys(data).length; index++) { 
-                    
-                    renderSite(data[index]);
-                } 
+                $(".rendered").remove();
+                data.forEach (function(site) { 
+                    renderSite(site);
+                })
             }
         }
     );
 }
 
 function renderSite(site){
-    site = ``
+    siteString = `
+        <div  class="card  float-sm-left border-success lg-3 md-4 sm-2 rendered" >
+            <div class="card-header"><a href="https://${site.domain}">${site.domain}</a></div>
+            <div class="card-body">
+                <h4 class="card-title">${site.title}</h4>
 
-    console.log(site); 
+                <h6 class="card-text">${site.source} -> ${site.target}</p>
+                <h6 class="card-text">tls: ${site.email}</p>
+                <p class="card-text">${site.description}</p>
+            </div>
+        </div>
+    `
+    document.getElementById("main").innerHTML += siteString;
+}
+
+function newSite(){
+    let json = {};
+    json["domain"] = document.getElementById('domain').value;
+    json["title"] = document.getElementById('title').value;
+    json["description"] = document.getElementById('description').value;
+    json["source"] = document.getElementById('source').value;
+    json["target"] = document.getElementById('target').value;
+    json["email"] = document.getElementById('email').value;
+    postJSON("http://localhost:8000/api/Sites", json, loadSites);
 }
